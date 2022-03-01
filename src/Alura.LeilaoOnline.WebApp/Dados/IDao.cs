95beb04a004d153e39ef57b613cdcca26040d732
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2022-03-01 14:04:11 -0300

    Segregação de Interfaces

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/IDao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public interface IDao<T>
+    {
+
+        IEnumerable<T> BuscarTodos();
+
+        T BuscarPorId(int id);
+
+        void Incluir(T obj);
+
+        void Alterar(T obj);
+
+        void Excluir(T obj);
+    }
+}
